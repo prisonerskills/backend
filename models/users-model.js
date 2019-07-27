d72d7b8c9c6e600009f9ec7b873556a2c4f7d6d6
Michael Mangus <mjmangus16@yahoo.com>
2019-07-27 06:13:13 -0400

    created .env file, registration and login is functional

--- models/users-model.js
+++ models/users-model.js
@@ -1,9 +1,46 @@
 const db = require("../data/data-config");
 
 module.exports = {
-  getAll
+  getAll,
+  findById,
+  add,
+  findByUsername
 };
 
 function getAll() {
   return db("users");
+}
+
+function findById(id) {
+  return db("users")
+    .where({ id })
+    .first()
+    .then(user => {
+      if (user) {
+        return user;
+      } else {
+        return null;
+      }
+    });
+}
+
+function findByUsername(username) {
+  return db("users")
+    .where({ username: username })
+    .first()
+    .then(user => {
+      if (user) {
+        return user;
+      } else {
+        return null;
+      }
+    });
+}
+
+function add(user) {
+  return db("users")
+    .insert(user, "id")
+    .then(id => {
+      return findById(...id);
+    });
 }
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,10 +1,76 @@
 const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const jwtKey = process.env.JWT_SECRET;
 
 const Users = require("../models/users-model");
 
 router.get("/", (req, res) => {
   Users.getAll().then(users => {
     res.status(200).json(users);
+  });
+});
+
+router.post("/register", (req, res) => {
+  const newUser = req.body;
+
+  Users.findByUsername(newUser.username).then(userFound => {
+    if (userFound !== null) {
+      return res.status(400).json({ message: "That username already exists!" });
+    } else {
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          Users.add(newUser)
+            .then(user => {
+              return res.status(200).json(user);
+            })
+            .catch(err =>
+              res.status(400).json({
+                message:
+                  "Something went wrong when adding the user to the database"
+              })
+            );
+        });
+      });
+    }
+  });
+});
+
+router.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  Users.findByUsername(username).then(userFound => {
+    if (userFound === null) {
+      return res.status(404).json({ username: "User does not exist" });
+    } else {
+      bcrypt
+        .compare(password, userFound.password)
+        .then(isMatch => {
+          if (isMatch) {
+            const payload = {
+              id: userFound.id,
+              username: userFound.username
+            };
+            console.log(jwtKey);
+            // Sign Token
+            jwt.sign(payload, jwtKey, { expiresIn: 7200 }, (err, token) => {
+              res.status(200).json({
+                success: true,
+                token: token
+              });
+            });
+          } else {
+            res.status(401).json({ message: "Invalid Credentials" });
+          }
+        })
+        .catch(error => {
+          res.status(500).json(error);
+        });
+    }
   });
 });
 
